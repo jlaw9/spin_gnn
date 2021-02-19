c7407984438c13b28079a454918e223eb26bac59
Jeffrey Law <jlaw@ed2.ib0.cm.hpc.nrel.gov>
2021-02-19 12:29:22 -0700

    Code to build a learning curve for the new DFT calculations

---
+++ 20210208_redox_rerun_new_calcs_lc/gpu_submit_redox.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --account=rlmolecule
+#SBATCH --time=12:00:00
+#SBATCH --job-name=new_redox_lc
+#SBATCH --nodes=3
+#SBATCH --gres=gpu:2
+# TODO why use scratch for the log file?
+#SBATCH --output=/scratch/jlaw/rlmolecule/20210218_redox_lc/2021-02-18-gpu.%j.out
+# Can't use bash variables inside of the SBATCH options
+#model_name="20210216_spin_bv_lc"
+#outputs_dir="outputs/$model_name/log"
+#mkdir -p outputs_dir
+#curr_date=`date +%F`
+##SBATCH --output=$outputs_dir/$curr_date-gpu.%j.out
+
+source /nopt/nrel/apps/anaconda/5.3/etc/profile.d/conda.sh
+module load cudnn/7.6.1  # ensure the correct drivers are loaded to use the GPUs
+conda activate /projects/rlmolecule/jlaw/envs/tf2_gpu
+
+echo "Started at `date`"
+echo `which python`
+
+for ((i = 0 ; i < 6 ; i++)); do
+    srun -l -n 1 --gres=gpu:1 --nodes=1 python train_model_redox.py $i &
+done
+
+wait
+
+echo "Finished at `date`"
